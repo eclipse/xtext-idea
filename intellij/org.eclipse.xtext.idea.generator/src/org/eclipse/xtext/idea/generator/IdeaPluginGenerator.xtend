vgirishkumar/xtext

[472592][idea] Implement DocumentationProvider
Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

intellij/org.eclipse.xtext.idea.generator/src/org/eclipse/xtext/idea/generator/IdeaPluginGenerator.xtend
@@ -443,6 +443,7 @@ class IdeaPluginGenerator extends Xtend2GeneratorFragment {
 		      	<pom.declarationSearcher implementation="«grammar.pomDeclarationSearcherName»"/>
 
 		      	«grammar.compileExtension('lang.psiStructureViewFactory', 'com.intellij.lang.PsiStructureViewFactory')»
+		      	«grammar.compileExtension('lang.documentationProvider', 'org.eclipse.xtext.idea.documentation.XtextDocumentationProvider')»
 				<facetType implementation="«grammar.facetTypeName»"/>
 				<colorSettingsPage implementation="«grammar.colorSettingsPage»"/>
 				<highlightVisitor implementation="«grammar.semanticHighlightVisitorName»"/>

eclipse/xtext-idea

intellij/org.eclipse.xtext.idea.generator/src/org/eclipse/xtext/idea/generator/IdeaPluginGenerator.xtend
@@ -446,6 +446,7 @@ class IdeaPluginGenerator extends Xtend2GeneratorFragment {
 		      	<pom.declarationSearcher implementation="«grammar.pomDeclarationSearcherName»"/>
 
 		      	«grammar.compileExtension('lang.psiStructureViewFactory', 'com.intellij.lang.PsiStructureViewFactory')»
+		      	«grammar.compileExtension('lang.documentationProvider', 'org.eclipse.xtext.idea.documentation.IdeaDocumentationProvider')»
 				<facetType implementation="«grammar.facetTypeName»"/>
 				<colorSettingsPage implementation="«grammar.colorSettingsPage»"/>
 				<highlightVisitor implementation="«grammar.semanticHighlightVisitorName»"/>